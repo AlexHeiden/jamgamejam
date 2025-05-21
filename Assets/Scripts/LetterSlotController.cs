8a2c27823693bf1d67252f0132bf3bedba7e6dcb
Aleksei Stepanov <prometey.titanov@gmail.com>
2025-05-21 11:22:59 +0200

    Create LetterSlotController

---
+++ Assets/Scripts/LetterSlotController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class LetterSlotManager : MonoBehaviour
+{
+    public List<Button> letterButtons;
+
+    private List<char> currentSequence = new List<char>();
+    private List<char> displayedSequence = new List<char>(); // предыдущее состояние
+    private int currentIndex = 0;
+
+    void Update()
+    {
+        // 🟡 Пример: получаем новые данные каждый кадр
+        List<int> inputFromBackend = SequenceMaster.Instance.GetSequence();
+
+        foreach (int num in inputFromBackend)
+        {
+            char letter = (char)('a' + num - 1);
+            currentSequence.Add(letter);
+        }
+        //UpdateLetterSlots(inputFromBackend);
+        //HandleKeyboardInput();
+    }
+}
